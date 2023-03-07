abb53c52907effbd250e7cb021ac78d8a65d5b3b
joelkariyalil1 <joeltc071@gmail.com>
2023-03-07 23:40:35 +0530

    Final Commit

---
+++ Code.py
@@ -0,0 +1,25 @@
+import math
+n=int(input('\nEnter the Cups (No.): '))
+
+#Calculating the Roots using the Quadratic Equation
+a=1;b=1;c=-2*n
+d = (b**2) - (4*a*c)   
+sol1 = (-b-math.sqrt(d))/(2*a)
+sol2 = (-b+math.sqrt(d))/(2*a)
+
+#Checking for Complex Numbers
+if isinstance(sol1, complex):
+    sol1=sol1.real
+if isinstance(sol2, complex):
+    sol2=sol2.real
+
+#Discarding the Negative Values
+ans=math.floor(sol2) if sol1<0 else math.floor(sol1)
+
+val=ans*(ans+1)//2
+
+#Printing the Results
+print('\nResults: \n')
+print('The number of Cups at the Base: ',ans)
+print('The number of Cups Used: ',val)
+print('Cups Remaining: ',n-val)   
